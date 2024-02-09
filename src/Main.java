1f372be6b35b1a29a0d13a8b00b3cee5bc057bfb
Tapiiaa <ptapilob@myuax.com>
2024-02-09 15:23:27 +0100

    Primer commit, Ejs1 y 2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,14 @@
+//Ejercicio 1: se inizializa una variable  con valor 3, y al ser un modelo predictivo
+//llegamos a la conclusión de que los valores de entrada no cambiará, siendo los modificados
+//los valores de salida.
+
+//---------------------------------------------------------------------------------------------------
+
+// Ejercicio 2: Operaciones en Machine Learning - Interpretación de Resultados:
+//En este ejercicio las operaciones y las variables están relacionadas con un modelo machine learning
+//En ML (Machine Learning) las variables pueden representar inputs del modelo, o resultados del modelo.
+//Poniendo un ejemplo de un hotel, "valor1" podría representar el tamaño del mismo, mientras que "valor2" representaría la cantidad de habitaciones.
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
