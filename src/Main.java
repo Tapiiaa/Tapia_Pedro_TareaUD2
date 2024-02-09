046a898b8ad70742a073f3e88b014da91e20d459
Tapiiaa <ptapilob@myuax.com>
2024-02-09 15:29:32 +0100

    Ejercicio 3

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,12 @@
 //En ML (Machine Learning) las variables pueden representar inputs del modelo, o resultados del modelo.
 //Poniendo un ejemplo de un hotel, "valor1" podría representar el tamaño del mismo, mientras que "valor2" representaría la cantidad de habitaciones.
 
+//------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//Ejercicio 3: Tanto  la precicisión como la correción de la manipulación de datos son fundamentales en el desarrollo de modelos de IA.
+//Esto se debe a que los datos alimentan estos modelos, además de determinar su eficiencia y precisión.
+//Por ejemplo, si se tienen datos incorrectos, el modelo no podrá predecir de manera correcta, y si se tienen datos imprecisos, el modelo no podrá predecir de manera eficiente.
+//Para la mejoría de IA, usaría datos que previamente estuvieran verificados y revisados, además de hacer una revisión lógica del algoritmo.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
